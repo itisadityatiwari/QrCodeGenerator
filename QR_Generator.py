df10ee9dba1c41646a771d97c4cbbe96916b1b48
Aditya Tiwari <atiwa007@fiu.edu>
2020-08-09 22:05:48 -0500

    Version 1.3

--- QR_Generator.py
+++ QR_Generator.py
@@ -7,13 +7,13 @@ def input_validation():
     Web_url=input("Enter Your URL:")
     valid=validators.url(Web_url)
     if valid==True:
-        print("Valid")
+        print("Your Input Url was Valid")
         link = qr.create(Web_url)
         link.png('qr_code.png', scale=8)
         img = cv.imread('qr_code.png', 0)
         cv.imshow('qr_code.png', img)
         cv.waitKey(0)
     else:
-        print("Your input was Invalid")
+        print("Your input  Url was Invalid")
         exit()
 input_validation()
